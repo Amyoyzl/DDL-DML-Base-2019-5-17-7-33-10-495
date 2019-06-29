7af535b5ef54744eca55b167d65c4d78cd07eb12
Amyoyzl <1421790940@qq.com>
2019-06-29 18:30:14 +0800

    edit sql

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,34 @@
--- **数据库级别：**  
+﻿-- **数据库级别：**  
 --  显示所有数据库  
-
+show databases;
 --  进入某个数据库  
-
+use student_examination_sys;
 --  创建一个数据库  
-
+create database if not exists student_examination_sys;
 --  创建指定字符集的数据库  
-
+create database if not exists student_examination_sys default character set = 'utf8';
 --  显示数据库的创建信息   
-
+show create database student_examination_sys;
 --  修改数据库的编码  
-
---  删除一个数据库   
+alter database student_examination_sys default character set gbk;
+--  删除一个数据库 
+drop database student_examination_sys;  
 
 -- **表级别**
 --  修改表名
-
+rename table student to student_update;
+alter table student_update rename to student;
 --  修改字段的数据类型
-
+alter table student modify column sex char(2);
 --  修改字段名
-
+alter table student change age age_update int;
 --  添加字段
-
+alter table student add address varchar(45);
 --  删除字段
-
+alter table student drop address;
 --  修改表的存储引擎
-
+alter table student engine=InnoDB;
 --  删除表的外键约束
-
+alter table score drop foreign key score_ibfk_1;
 --  删除一张表
+drop table score;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
--- 插入记录  
-
+﻿-- 插入记录  
+insert into student values('003','王五',19,'男');
+insert into student (id,name) values('004','赵六');
 -- 修改记录  
-
+update student set name = '赵四' where id = '004';
 --  删除记录  
-
--- 查询记录  
+delete from student where id = '004';
+-- 查询记录 
+select * from student; 
